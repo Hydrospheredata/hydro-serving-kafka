0314c3a1d548355bdb65833b96c5eacd1ddf12bc
dmitryisaev <dmitryisaev@cloud.upwork.com>
2018-02-19 16:52:20 +0300

    added wait-for-manager script

--- src/test/scala/io/hydrosphere/serving/kafka/it/KafkaSpec.scala
+++ src/test/scala/io/hydrosphere/serving/kafka/it/KafkaSpec.scala
@@ -1,31 +1,31 @@
-package io.hydrosphere.serving.kafka.it
-
-import com.whisk.docker.impl.spotify.DockerKitSpotify
-import com.whisk.docker.scalatest.DockerTestKit
-import io.hydrosphere.serving.kafka.it.infrostructure.KafkaContainer
-import org.scalatest.{time, _}
-import org.scalatest.time._
-
-import scala.concurrent.duration._
-import scala.concurrent.Await
-
-class KafkaSpec extends FlatSpec
-  with Matchers
-  with KafkaContainer
-  with BeforeAndAfter {
-
-  "kafka container" should "topic should be created" in {
-    val topicName = "test"
-    topicExists(topicName) shouldBe false
-    createTopic(topicName)
-    topicExists(topicName) shouldBe true
-  }
-
-  "kafka container" should "topic should be deleted" in {
-    val topicName = "test"
-    topicExists(topicName) shouldBe false
-    createTopic(topicName)
-    topicExists(topicName) shouldBe true
-  }
-}
-
+//package io.hydrosphere.serving.kafka.it
+//
+//import com.whisk.docker.impl.spotify.DockerKitSpotify
+//import com.whisk.docker.scalatest.DockerTestKit
+//import io.hydrosphere.serving.kafka.it.infrostructure.KafkaContainer
+//import org.scalatest.{time, _}
+//import org.scalatest.time._
+//
+//import scala.concurrent.duration._
+//import scala.concurrent.Await
+//
+//class KafkaSpec extends FlatSpec
+//  with Matchers
+//  with KafkaContainer
+//  with BeforeAndAfter {
+//
+//  "kafka container" should "topic should be created" in {
+//    val topicName = "test"
+//    topicExists(topicName) shouldBe false
+//    createTopic(topicName)
+//    topicExists(topicName) shouldBe true
+//  }
+//
+//  "kafka container" should "topic should be deleted" in {
+//    val topicName = "test"
+//    topicExists(topicName) shouldBe false
+//    createTopic(topicName)
+//    topicExists(topicName) shouldBe true
+//  }
+//}
+//
